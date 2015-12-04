a102c4e7b8a1df17a72378acd0e273139290e32f
Znurre <znurree@gmail.com>
2015-12-04 18:20:37 +0100

    Started working on the search page

---
+++ sailfish/src/SearchModel.cpp
@@ -0,0 +1,5 @@
+#include "SearchModel.h"
+
+SearchModel::SearchModel()
+{
+}
---
+++ sailfish/src/SearchModel.h
@@ -0,0 +1,10 @@
+#ifndef SEARCHMODEL_H
+#define SEARCHMODEL_H
+
+class SearchModel
+{
+	public:
+		SearchModel();
+};
+
+#endif // SEARCHMODEL_H
